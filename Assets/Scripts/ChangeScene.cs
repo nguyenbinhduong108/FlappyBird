2d3dcb509d33a684ee1e120afc4fcbfae5cfb45c
nguyenbinhduong108 <binhduong10082002@gmail.com>
2023-04-15 01:27:12 +0700

    Change Scene by Hour of Day

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int Date = DateTime.Today.Hour;
+
+    private void Update()
+    {
+        if (Date <= 7 && Date >= 0 || Date >= 18 && Date < 24)
+        {
+            SceneManager.LoadScene("gamePlayNight");
+            Debug.Log("Chuyen dem " + Date);
+        }
+        if (Date > 7 && Date < 18)
+        {
+            SceneManager.LoadScene("gamePlayDay");
+            Debug.Log("Chuyen ngay " + Date);
+        }
+    }
+}
+
