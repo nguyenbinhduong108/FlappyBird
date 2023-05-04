85987931efd0a520d3ac2fb9efcc46e6fe4dd127
AnhSenPie <121533157+AnhSenPie@users.noreply.github.com>
2023-05-04 16:10:36 +0700

    Xu ly va cham va hien thi diem so

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Bird : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
     public float jumpForce;
-
+    private bool gameStart;
+    public GameObject gameController;
+    private int score;
+    public Text scoreText;
     private void Awake()
     {
         rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+        gameStart = false; 
+        rigidbody.gravityScale = 0; 
+        score = 0;
+        scoreText.text = score.ToString();
     }
 
     
@@ -19,7 +28,12 @@ public class Bird : MonoBehaviour
     {
         // Kiem tra phim space co duoc bam khong
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+        {   
+            if(gameStart == false){
+                gameStart = true;
+                rigidbody.gravityScale = 8; 
+                gameController.GetComponent<PipeGenerator>().enablePipe = true;
+            }
             BirdMoveUp();
         }
     }
@@ -27,5 +41,17 @@ public class Bird : MonoBehaviour
     private void BirdMoveUp() // bay len
     {
         rigidbody.velocity = Vector2.up * jumpForce;
+    }
+    private void OnCollisionEnter2D(Collision2D collision){
+       ReloadScene();
+       score = 0;
+       scoreText.text = score.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        score += 1;
+        scoreText.text = score.ToString();
+    }
+    public void ReloadScene(){
+        SceneManager.LoadScene("gamePlayDay");
     }
 }
--- Assets/Scripts/PipeGenerator.cs
+++ Assets/Scripts/PipeGenerator.cs
@@ -7,20 +7,24 @@ public class PipeGenerator : MonoBehaviour
     public GameObject pipePrefab;
     private float countdown;
     public float timeDuration;
+    public bool enablePipe;
 
     private void Awake()
     {
         countdown = timeDuration;
+        enablePipe = false; 
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if(enablePipe == true){
         countdown -= Time.deltaTime;
         if (countdown <= 0)
         {
             Instantiate(pipePrefab, new Vector3(10, Random.Range(-2.5f,-0.5f), 0), Quaternion.identity);
             countdown = timeDuration;
+        }
         }
     }
 }
