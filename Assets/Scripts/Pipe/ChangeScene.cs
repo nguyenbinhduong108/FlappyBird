2b7a99447636b24a45c8ec2bee08f1422a346949
TungNguyen0510 <88915666+TungNguyen0510@users.noreply.github.com>
2023-05-29 07:55:28 +0700

    fix ChangeScene by time

--- Assets/Scripts/Pipe/ChangeScene.cs
+++ Assets/Scripts/Pipe/ChangeScene.cs
@@ -20,12 +20,12 @@ public class ChangeScene : MonoBehaviour
 
     private void Change()
     {
-        if (Date <= 7 && Date >= 0 || Date >= 18 && Date < 24)
+        if (Date < 7 && Date >= 0 || Date >= 18 && Date < 24)
         {
             SceneManager.LoadScene("gamePlayNight");
             /*Debug.Log("Chuyen dem " + Date);*/
         }
-        if (Date > 7 && Date < 18)
+        if (Date >= 7 && Date < 18)
         {
             SceneManager.LoadScene("gamePlayDay");
             /*Debug.Log("Chuyen ngay " + Date);*/
